c9a7ecbfc04c43e3971d649b265efa5840bcfb49
Danil <vasiljev.dani1@yandex.ru>
2023-12-22 14:51:57 +0700

    asd

---
+++ PR31/app/src/main/java/com/example/pr31/ThirdActivity.java
@@ -0,0 +1,35 @@
+package com.example.pr31;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Bundle;import android.view.View;
+import android.widget.Button;import androidx.appcompat.app.AppCompatActivity;
+public class ThirdActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ThirdActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
